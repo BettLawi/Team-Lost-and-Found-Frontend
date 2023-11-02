182fe81b9778c07216c703b4e5795d074fa5287a
Vany <vany.ikuze@student.moringaschool.com>
2023-11-02 09:50:30 +0300

    Updated Footer.jsx

--- Lost-and-found-footer/src/Footer.jsx
+++ Lost-and-found-footer/src/Footer.jsx
@@ -6,25 +6,21 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-content">
         <div className="contact-info">
-          <p>Contact Us: <a href="mailto:lostandfound@gmail.com">lostandfound@gmail.com</a></p>
+          <p>Contact Us: <a href="mailto:lostfound656@gmail.com">lostfound656@gmail.com</a></p>
           <p>Contact Number: <a href="tel:+254718534285">+254 718 534 285</a></p>
         </div>
         <div className="social-media">
-          <a href="https://www.facebook.com/your-facebook-page" className="social-link">
+          <a href="https://www.facebook.com/profile.php?id=61552465873026" className="social-link">
             <span>Facebook</span>
             <i className="fab fa-facebook"></i>
           </a>
-          <a href="https://twitter.com/your-twitter" className="social-link">
+          <a href="https://twitter.com/lostfound656" className="social-link">
             <span>Twitter</span>
             <i className="fab fa-twitter"></i>
           </a>
-          <a href="https://www.instagram.com/your-instagram" className="social-link">
+          <a href="https://www.instagram.com/lostfound656/" className="social-link">
             <span>Instagram</span>
             <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://www.linkedin.com/in/your-linkedin" className="social-link">
-            <span>LinkedIn</span>
-            <i className="fab fa-linkedin"></i>
           </a>
         </div>
       </div>
