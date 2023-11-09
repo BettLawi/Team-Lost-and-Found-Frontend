0373f7e815c5e1cb672b0942d834dc870bcc8b21
user <mutukukennedy34@gmail.com>
2023-11-10 00:50:50 +0300

    mm

--- src/PendingClaims.jsx
+++ src/PendingClaims.jsx
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import './PendingClaims.css';
 
 import Swal from 'sweetalert2';
